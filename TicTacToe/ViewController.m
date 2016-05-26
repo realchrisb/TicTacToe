06ba020adc2b0a819ef82c98e5483f8b0c4d04bc
Christopher Benavides <chrisbreezy@Christophers-MacBook-Pro.local>
2016-05-26 14:37:23 -0500

    Message

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -9,10 +9,39 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *ButtonOne;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonTwo;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonThree;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonFour;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonFive;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonSix;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonSeven;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonEight;
+@property (weak, nonatomic) IBOutlet UIButton *ButtonNine;
+
+@property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 
 @end
 
 @implementation ViewController
+
+
+- (IBAction)onButtonTapped:(UIButton *)sender {
+    self.whichPlayerLabel.text = [NSString stringWithFormat:@"X"];
+    
+    if ((self.whichPlayerLabel.text = @"X")) {
+        self.whichPlayerLabel.text = @"Y";
+    } else {
+        self.whichPlayerLabel.text = @"X";
+    }
+    
+    
+
+
+
+
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
